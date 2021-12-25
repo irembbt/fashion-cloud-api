8e5548f45e8003a14eb74b841bd9497915da66ac
irembbt <irembulut_97@hotmail.com>
2021-12-25 14:12:56 +0300

    Add response tests

--- test/cache.test.js
+++ test/cache.test.js
@@ -7,7 +7,7 @@ chai.should();
 
 describe('Cache', () => {
   describe('GET Operations', () => {
-    it('should get all keys in cache', (done) => {
+    it('should indicate no content', (done) => {
       chai.request(app)
         .get('/cache')
         .end((err, res) => {
@@ -15,5 +15,79 @@ describe('Cache', () => {
           done();
         })
     });
-  })
+
+    it('should put a random value to key', (done) => {
+      const key = 1;
+
+      chai.request(app)
+        .get(`/cache/${key}`)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          done();
+        })
+    });
+
+    it('should get all keys in cache', (done) => {
+      chai.request(app)
+        .get('/cache')
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        })
+    });
+
+  });
+
+  describe('PUT Operations', () => {
+    const key = 5;
+
+    it('should add a key if key does not exist', (done) => {
+      chai.request(app)
+        .put(`/cache/${key}`)
+        .send({ value: "abcdef" })
+        .end((err, res) => {
+          console.log(err);
+          console.log(res);
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          done();
+        })
+    });
+
+    it('should update an existing key', (done) => {
+      chai.request(app)
+        .put(`/cache/${key}`)
+        .send({ value: "updated val for key 5" })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        })
+    });
+  });
+
+  describe('DELETE Operations', () => {
+    it('should delete a specific key', (done) => {
+      const key = 1;
+
+      chai.request(app)
+        .delete(`/cache/${key}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        })
+    });
+
+    it('should delete all keys', (done) => {
+      chai.request(app)
+        .delete('/cache')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
+        })
+    });
+  });
 });
